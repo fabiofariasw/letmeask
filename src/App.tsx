5086f62c0382a7f61c8b0fdf8d9b73a06dc14528
Fabio Farias <64112026+fabiofariasw@users.noreply.github.com>
2021-06-23 18:08:54 -0300

    feature/adição de context

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { Button } from './components/Button'
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-  // <h1>hello world</h1>
-    <Button text="Botão 1"/>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
