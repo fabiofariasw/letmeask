a4e873c772e4538d767d4e1467c17d227f46c042
Fabio Farias <fabiofariasw@gmail.com>
2021-10-21 23:48:42 -0300

    finalização do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Home } from './pages/Home';
-import { NewRoom } from './pages/NewRoom';
-import { Room } from './pages/Room';
-import { AuthContextProvider } from './contexts/AuthContext';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
